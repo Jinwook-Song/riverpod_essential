997c1999dbf0a72ea194b0b8687681bb51c0f32f
jinwook song <wlsdnr129@naver.com>
2024-05-07 23:34:51 +0900

    theme: theme provider

--- todo_riverpod_sync/lib/pages/widgets/todo_header.dart
+++ todo_riverpod_sync/lib/pages/widgets/todo_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_riverpod_sync/pages/providers/theme/theme_provider.dart';
 
 import '../providers/active_todo_count/active_todo_count_provider.dart';
 import '../providers/todo_list/todo_list_provider.dart';
@@ -13,19 +14,27 @@ class TodoHeader extends ConsumerWidget {
     final todos = ref.watch(todoListProvider);
 
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const Text(
-          'TODO',
-          style: TextStyle(fontSize: 36.0),
+        Row(
+          children: [
+            const Text(
+              'TODO',
+              style: TextStyle(fontSize: 36.0),
+            ),
+            const SizedBox(width: 10),
+            Text(
+              '($activeTodoCount/${todos.length} item${activeTodoCount != 1 ? "s" : ""} left)',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.blue[900],
+              ),
+            ),
+          ],
         ),
-        const SizedBox(width: 10),
-        Text(
-          '($activeTodoCount/${todos.length} item${activeTodoCount != 1 ? "s" : ""} left)',
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Colors.blue[900],
-          ),
-        ),
+        IconButton(
+            onPressed: ref.read(themeProvider.notifier).toggleTheme,
+            icon: const Icon(Icons.light_mode_outlined))
       ],
     );
   }
