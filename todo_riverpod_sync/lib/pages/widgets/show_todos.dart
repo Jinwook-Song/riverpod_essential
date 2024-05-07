433b81dd3890a630ecf5419460caa4e9d5954f65
jinwook song <wlsdnr129@naver.com>
2024-05-07 22:59:46 +0900

    todo: widgets

---
+++ todo_riverpod_sync/lib/pages/widgets/show_todos.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todo_riverpod_sync/pages/widgets/todo_item.dart';
+
+import '../providers/filtered_todos/filtered_todos_provider.dart';
+
+class ShowTodos extends ConsumerWidget {
+  const ShowTodos({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final filteredTodos = ref.watch(filteredTodosProvider);
+
+    return ListView.separated(
+      itemCount: filteredTodos.length,
+      separatorBuilder: (BuildContext context, int index) {
+        return const Divider(color: Colors.grey);
+      },
+      itemBuilder: (BuildContext context, int index) {
+        final todo = filteredTodos[index];
+        return TodoItem(todo: todo);
+      },
+    );
+  }
+}
